0645dfb30138094217cc310e24cd3dfa50324d43
Mateus Costa <mcmateus070@gmail.com>
2025-09-04 13:28:18 -0300

    retirando coments

--- src/app/models/cliente.update.ts
+++ src/app/models/cliente.update.ts
@@ -9,21 +9,15 @@ export interface ClientePut {
   rg_ie: string;
   tipo_regime_apuracao: string;
   tipo_preco_venda: string;
-
-  // flags e status
   ativo: boolean;
   chk_emp_disponivel: boolean;
   desconto_auto_aplicar: boolean;
   consumidor_final: boolean;
-
-  // relações
   emp_id: number;
   cadastro_id: number;
   cadastro_empresa_id: number;
   cadastro_empresa_guid: string;
-
-  // metadados
   guid: string;
-  dt_inclusao: string; // pode tipar como Date se converter
+  dt_inclusao: string;
   usuario_inclusao_id: number;
 }
